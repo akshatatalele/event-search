d9fc56db7ad5d0e800145e5ff3c5ecb462b7baf2
akshatatalele <akshata.talele@gmail.com>
2021-05-31 22:34:40 -0700

    Added CSS file and updated html

---
+++ Homework06/main.py
@@ -0,0 +1,13 @@
+from flask import Flask, redirect, url_for, render_template, request, flash
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'webtechnologieshomework'
+
+@app.route("/")
+def index():
+    message1 = "Hello World Akshata"
+    return render_template("home.html", message1= message1)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
