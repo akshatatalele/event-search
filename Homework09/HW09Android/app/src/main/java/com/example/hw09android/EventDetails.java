cb617bd5066334eeab0d056158650e00f33b0266
akshatatalele <akshata.talele@gmail.com>
2021-06-25 02:01:50 -0700

    Added models

---
+++ Homework09/HW09Android/app/src/main/java/com/example/hw09android/EventDetails.java
@@ -0,0 +1,14 @@
+package com.example.hw09android;
+
+public class EventDetails {
+    String ID;
+    String Name;
+    String Artists;
+    String Venue;
+    String Date;
+    String Category;
+    String PriceRange;
+    String TicketStatus;
+    String BuyTicketAt;
+    String SeatMap;
+}
---
+++ Homework09/HW09Android/app/src/main/java/com/example/hw09android/EventTable.java
@@ -0,0 +1,11 @@
+package com.example.hw09android;
+
+public class EventTable {
+    String ID;
+    String Date;
+    String Name;
+    String Category;
+    String Venue;
+    String isFavorite;
+    String tooltip;
+}
