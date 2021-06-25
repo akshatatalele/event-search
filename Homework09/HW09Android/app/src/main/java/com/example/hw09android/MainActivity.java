930a880680bb654fb5cb540d095e842294163f9d
akshatatalele <akshata.talele@gmail.com>
2021-06-25 01:50:42 -0700

    Added Search and Favorites tab to MainActivity

--- Homework09/HW09Android/app/src/main/java/com/example/hw09android/MainActivity.java
+++ Homework09/HW09Android/app/src/main/java/com/example/hw09android/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.hw09android;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentTransaction;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -15,18 +16,55 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
+import com.google.android.material.tabs.TabLayout;
 
 import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
     RequestQueue requestQueue;
+    TabLayout tabLayout;
+    ViewPager2 viewPager2;
+    SearchFravoriteFragmentAdapter searchFravoriteFragmentAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        tabLayout = findViewById(R.id.ID_SearchForm_tablayout);
+        viewPager2 = findViewById(R.id.ID_SearchForm_ViewPager);
+
+        searchFravoriteFragmentAdapter = new SearchFravoriteFragmentAdapter(getSupportFragmentManager(), getLifecycle());
+        viewPager2.setAdapter(searchFravoriteFragmentAdapter);
+
+        tabLayout.addOnTabSelectedListener(
+                new TabLayout.OnTabSelectedListener() {
+                    @Override
+                    public void onTabSelected(TabLayout.Tab tab) {
+                        viewPager2.setCurrentItem(tab.getPosition());
+                    }
+
+                    @Override
+                    public void onTabUnselected(TabLayout.Tab tab) {
+
+                    }
+
+                    @Override
+                    public void onTabReselected(TabLayout.Tab tab) {
+
+                    }
+                }
+        );
+
+        viewPager2.registerOnPageChangeCallback(
+                new ViewPager2.OnPageChangeCallback() {
+                    @Override
+                    public void onPageSelected(int position) {
+                        tabLayout.selectTab(tabLayout.getTabAt(position));
+                    }
+                }
+        );
         Button btn = (Button)findViewById(R.id.button2);
         btn.setOnClickListener(
                 new View.OnClickListener() {
